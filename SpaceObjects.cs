9c5cb99c27f5a97a77527b3a4c8724054a62f390
Maria <maria.danborg@hotmail.com>
2021-09-16 09:08:21 +0200

    Add project files.

---
+++ SpaceObjects.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaticaMOD
+{
+    abstract class SpaceObjects
+    {
+        // declare and instantiante the enum Startype
+        public enum Startype { YellowDwarf, White, BlueNeutron, RedGiant }
+        // declare properties Id and Name
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // nested class position
+        class Position
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            // overrides Position and returns the values (x,y)
+            public override string ToString()
+            {
+                return "(" + x + "," + y + ")";
+            }
+        }
+    }
+}
---
+++ Star.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaticaMOD
+{
+    // the class Star inherits from SpaceObjects
+    class Star: SpaceObjects
+    {
+
+    }
+}
